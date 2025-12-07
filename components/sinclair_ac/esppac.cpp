b642fabba7f975bd8d9608154ba39dd211b0aea0
Zsolt Komlósy <komlosy@gmail.com>
2025-12-07 19:36:07 +0100

    dev

--- components/sinclair_ac/esppac.cpp
+++ components/sinclair_ac/esppac.cpp
@@ -23,6 +23,7 @@ climate::ClimateTraits SinclairAC::traits()
     traits.set_supported_modes({climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_AUTO, climate::CLIMATE_MODE_COOL,
                                 climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_FAN_ONLY, climate::CLIMATE_MODE_DRY});
 
+    /*
     traits.add_supported_custom_fan_mode(fan_modes::FAN_AUTO);
     //traits.add_supported_custom_fan_mode(fan_modes::FAN_QUIET);
     traits.add_supported_custom_fan_mode(fan_modes::FAN_LOW);
@@ -31,6 +32,17 @@ climate::ClimateTraits SinclairAC::traits()
     //traits.add_supported_custom_fan_mode(fan_modes::FAN_MEDH);
     traits.add_supported_custom_fan_mode(fan_modes::FAN_HIGH);
     traits.add_supported_custom_fan_mode(fan_modes::FAN_TURBO);
+    */
+    traits.set_supported_custom_fan_modes({	
+        fan_modes::FAN_AUTO,	
+        fan_modes::FAN_QUIET,	
+        fan_modes::FAN_LOW,	
+        fan_modes::FAN_MEDL,	
+        fan_modes::FAN_MED,	
+        fan_modes::FAN_MEDH,	
+        fan_modes::FAN_HIGH,	
+        fan_modes::FAN_TURBO	
+    });
 
     traits.set_supported_swing_modes({climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_BOTH,
                                       climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL});
